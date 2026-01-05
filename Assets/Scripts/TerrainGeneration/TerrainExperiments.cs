030e6bb9cc595cb8fc45862dedc9a1e9776193f1
Nikita Kurylev <n.kurylev@bestin.games>
2026-01-06 01:13:01 +0300

    feat: improve experiments and add new ones

--- Assets/Scripts/TerrainGeneration/TerrainExperiments.cs
+++ Assets/Scripts/TerrainGeneration/TerrainExperiments.cs
@@ -6,48 +6,84 @@ namespace TerrainGeneration
 {
     public class TerrainExperiments : MonoBehaviour
     {
-        private const string FileName = "test.csv";
-        private const int TestsPerStep = 3;
+        private const int TestsPerStep = 5;
 
         [SerializeField] private TerrainManager _terrain;
-        [SerializeField] private TerrainGenerationSettings _settings;
+        [SerializeField] private TerrainGenerationSettings _threeBiomes;
+        [SerializeField] private TerrainGenerationSettings _sevenBiomes;
+        
+        private TerrainGenerationSettings _settings;
 
         [InspectorButton("StartExperiments")] public bool _startExperiments;
 
         void StartExperiments()
         {
-            if(File.Exists(FileName))
-                File.Delete(FileName);
+            _settings = _threeBiomes;
+            RunBiomeSizeExperiment("biomeSize3biomes.csv");
+            _settings = _sevenBiomes;
+            RunBiomeSizeExperiment("biomeSize7biomes.csv");
+
+            _settings = _threeBiomes;
+            RunTerrainSizeExperiment("terrainSize3biomes.csv");
+            _settings = _sevenBiomes;
+            RunTerrainSizeExperiment("terrainSize7biomes.csv");
+        }
+
+        void RunBiomeSizeExperiment(string filename)
+        {
+            if(File.Exists(filename))
+                File.Delete(filename);
         
-            var file = File.CreateText(FileName);
-            file.WriteLine("# wfc perlin paint total");
+            var file = File.CreateText(filename);
+            file.WriteLine("biomeSize wfc perlin paint total");
 
-            for (int i = 16; i <= 1024; i += 16)
-            {
-
-                float avgWfc = 0, avgPerlin = 0, avgPaint = 0, avgTotal = 0;
-                for (int j = 0; j < TestsPerStep; j++)
-                {
-                    _terrain.GenerateTerrain(new MockTerrainGenerationSettings()
-                    {
-                        Size = i,
-                        Biomes = _settings.Biomes,
-                        BiomeSize = 32,
-                        InterpolationCurve = _settings.InterpolationCurve,
-                        OctaveScales = _settings.OctaveScales
-                    });
-
-                    avgWfc += _terrain.WfcTime;
-                    avgPerlin += _terrain.PerlinTime;
-                    avgPaint += _terrain.PaintTime;
-                    avgTotal += _terrain.TotalTime;
-                }
-
-                file.WriteLine($"{i} {avgWfc} {avgPerlin} {avgPaint} {avgTotal}");
-                file.Flush();
-            }
+            for (int i = 8; i <= 64; i += 2)
+                RunExperimentAndWriteResult(file, i, 1024, i);
         
             file.Close();
+        }
+
+        void RunTerrainSizeExperiment(string filename)
+        {
+            if(File.Exists(filename))
+                File.Delete(filename);
+        
+            var file = File.CreateText(filename);
+            file.WriteLine("size wfc perlin paint total");
+
+            for (int i = 16; i <= 1024; i += 16)
+                RunExperimentAndWriteResult(file, i, i, 32);
+        
+            file.Close();
+        }
+
+        void RunExperimentAndWriteResult(StreamWriter file, int i, int size, int biomeSize)
+        {
+            float avgWfc = 0, avgPerlin = 0, avgPaint = 0, avgTotal = 0;
+            for (int j = 0; j < TestsPerStep; j++)
+            {
+                _terrain.GenerateTerrain(new MockTerrainGenerationSettings()
+                {
+                    Size = size,
+                    Biomes = _settings.Biomes,
+                    BiomeSize = biomeSize,
+                    InterpolationCurve = _settings.InterpolationCurve,
+                    OctaveScales = _settings.OctaveScales
+                });
+
+                avgWfc += _terrain.WfcTime;
+                avgPerlin += _terrain.PerlinTime;
+                avgPaint += _terrain.PaintTime;
+                avgTotal += _terrain.TotalTime;
+            }
+
+            avgWfc /= TestsPerStep;
+            avgPerlin /= TestsPerStep;
+            avgPaint /= TestsPerStep;
+            avgTotal /= TestsPerStep;
+
+            file.WriteLine($"{i} {avgWfc} {avgPerlin} {avgPaint} {avgTotal}");
+            file.Flush();
         }
     }
 }
